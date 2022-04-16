549ebf27d58bb9ebd7093f5a2a0a1dac8316ff15
Tanya1621 <70881555+Tanya1621@users.noreply.github.com>
2022-04-16 13:17:11 +0300

    pre-final
    Before final

--- index.js
+++ index.js
@@ -21,3 +21,42 @@ cardsItems.forEach(function (element) {
     text.classList.add("play__text_opened");
   });
 });
+
+const popup = document.querySelector('.popup');
+const aboutCards = document.querySelectorAll('.about__card');
+const closeButton = document.querySelector('.popup__close');
+const text1 = 'Я родился в один из тех дней, когда хочется посильнее закутаться в одеялко и не под каким предлогом не выходить на улицу. Нас было четверо - я и три мои сестренки. Так и шла моя жизнь, я спал, ел, играл и воевал за территорию.'
+const text2 = 'Все изменилось, когда пришла она, от нее приятно пахло, она была нерасторопна и неуклюжа. Я сначала испугался, но любопытство взяло верх. Я подошел к ней и своим тоненьким голоском приказал меня почесать. Она отозвалась на мою просьбу и погладила мое пузико. Так Танюша стала моей мамой-кошкой.'
+let headingPopup = document.querySelector('.popup__heading');
+let text = document.querySelector('.popup__text');
+
+
+
+function openPopup () {
+  popup.classList.add('popup_opened');
+}
+
+function closePopup(){
+  popup.classList.remove('popup_opened');
+}
+
+closeButton.addEventListener('click', closePopup);
+
+aboutCards.forEach (function(element) {
+element.addEventListener('click', function () {
+  openPopup ();
+  const heading = element.querySelector('.about__heading').textContent;
+headingPopup.textContent = heading;
+switch (heading){
+  case 'Начало':
+    text.textContent = text1;
+ break;
+ case 'Встреча':
+  text.textContent = text2;
+ break;
+ case 'Новый дом':
+  text.textContent = text3;
+ break;
+}
+})
+});
