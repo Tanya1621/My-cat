027e76f63cad497c2627d21f9f3986d9f4a54a72
Tanya1621 <70881555+Tanya1621@users.noreply.github.com>
2022-04-23 23:39:54 +0300

    english
    -

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ cardsItems.forEach(function (element) {
 const popup = document.querySelector('.popup');
 const aboutCards = document.querySelectorAll('.about__card');
 const closeButton = document.querySelector('.popup__close');
-const text1 = 'Я родился в один из тех дней, когда хочется посильнее закутаться в одеялко и не под каким предлогом не выходить на улицу. Нас было четверо - я и три мои сестренки. Так и шла моя жизнь, я спал, ел, играл и воевал за территорию.'
-const text2 = 'Все изменилось, когда пришла она. Я сначала испугался, но любопытство взяло верх. Я подошел к ней и своим тоненьким голоском приказал меня почесать. Она отозвалась на мою просьбу и погладила мое пузико. Так Танюша стала моей мамой-кошкой.'
-const text3 = 'Придя домой, я почувствовал еще один слабый запах, который я решил проигнорировать и сразу занял кровать. Все было прекрасно, и я думал, что попал в рай, пока не приехал он. Самый добрый, ласковый, лучший друг котеек Коля.'
+const text1 = 'I was born on one of those days when you want to stay in your bed under a soft and warm blanket. There were four kittens - me and my sisters. My life went so: I ate, slept, played and fought for the area.'
+const text2 = 'Everything changed when she came. I was afraid for the first moment, but I was too curious. I came to her and ordered to pet me. She responded to my request and stroked my belly. So Tanya became my mother.'
+const text3 = 'When I came to my new home, I felt like a real host. I found my new place between pillows in a bed. Being a very humble cat, I left some space on the edge of the bed for my new mom.'
 let headingPopup = document.querySelector('.popup__heading');
 let text = document.querySelector('.popup__text');
 
@@ -49,13 +49,13 @@ element.addEventListener('click', function () {
   const heading = element.querySelector('.about__heading').textContent;
 headingPopup.textContent = heading;
 switch (heading){
-  case 'Начало':
+  case 'The beginning':
     text.textContent = text1;
  break;
- case 'Встреча':
+ case 'The meeting':
   text.textContent = text2;
  break;
- case 'Новый дом':
+ case 'New home':
   text.textContent = text3;
  break;
 }
